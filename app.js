e4de37970d7d64bd027c1fc565694ffe67acfe0b
Bhaskar Sinha <bhaskarsinha@Bhaskars-MacBook-Pro.local>
2017-03-05 20:44:29 -0500

    search implemented

--- app.js
+++ app.js
@@ -34,6 +34,24 @@ TeamApp.factory('Auth' , function($firebaseAuth) {
     return auth;
 });
 
+TeamApp.factory('Ref' , function ($firebaseArray , $firebaseObject) {
+
+    var usersRef = firebase.database().ref('');
+    var users = $firebaseArray(usersRef);
+    var Users = {
+        getProfile: function(uid){
+            return $firebaseObject(usersRef.child(uid));
+        },
+        getDisplayName: function(uid){
+            return users.$getRecord(uid).displayName;
+        },
+        all: users
+    };
+
+
+
+});
+
 
 //controllers
 TeamApp.controller('AuthCtrl' ,['$rootScope','$scope', '$window','Auth','$location' ,'$firebaseArray' , '$firebaseObject',
@@ -46,7 +64,6 @@ TeamApp.controller('AuthCtrl' ,['$rootScope','$scope', '$window','Auth','$locati
 
 // HELPER FUNCTION TO NAVIGATE TO PATH
         $scope.go = function (path) {
-            //console.log("in go");
             $location.path(path);
         };
 
@@ -54,17 +71,20 @@ TeamApp.controller('AuthCtrl' ,['$rootScope','$scope', '$window','Auth','$locati
             console.log("in Auth Changed");
             Auth.$onAuthStateChanged( function (user) {
 
-                console.log("Logged in as: " + user.uid);
-                $scope.uid = user.uid;
-                $scope.go("/"+$scope.uid);
-                const dbRef = firebase.database().ref();
-                const userRef = dbRef.child(user.uid);
-                $scope.object = $firebaseObject(userRef) ;
-                console.log($scope.object);
+                if(user === null){
+                    $scope.go("/");
 
-                //make database model
+                }
 
-
+                else {
+                    console.log("Logged in as: " + user.uid);
+                    $scope.uid = user.uid;
+                    $scope.go("/" + $scope.uid);
+                    const dbRef = firebase.database().ref();
+                    const userRef = dbRef.child(user.uid);
+                    $scope.object = $firebaseObject(userRef);
+                    console.log($scope.object);
+                }
             });
         };
 
@@ -82,19 +102,10 @@ TeamApp.controller('AuthCtrl' ,['$rootScope','$scope', '$window','Auth','$locati
         };
 
         $scope.createUser = function () {
-
-            $scope.user.uid = {
-                "email":"",
-                "name":"",
-                //COMPLETE THIS HOOK IT UPTO MODEL IN MODAL. CREATE USER IS DISCONNECTED
-
-            }
             console.log("In create Method");
             Auth.$createUserWithEmailAndPassword($scope.email , $scope.password).then(function (auth) {
                 console.log("User Created Successfully");
                 //OPEN FORM AND ADD SKILL
-
-
 
                 $scope.login();
             }, function (error) {
@@ -103,13 +114,10 @@ TeamApp.controller('AuthCtrl' ,['$rootScope','$scope', '$window','Auth','$locati
         };
     }]);
 
+
 TeamApp.controller('homeController' ,['$scope', '$route', '$routeParams', '$location' , function($scope , $route, $routeParams, $location) {
 
-    console.log("In home controller");
-
     $scope.param = $routeParams.id;
-    console.log($scope.param);
-
 
     // Fetching data from firebase:
 
@@ -119,66 +127,155 @@ TeamApp.controller('homeController' ,['$scope', '$route', '$routeParams', '$loca
 
     firebase.database().ref(userId+'/email').on('value', function(snapshot){
         $scope.email = snapshot.val();
-
-        //console.log(snapshot.val());
     });
+
     firebase.database().ref(userId+'/phone').on('value', function(snapshot){
         $scope.phone = snapshot.val();
-        //console.log(phone);
     });
+
     firebase.database().ref(userId+'/skills').on('value', function(snapshot){
         $scope.skills = snapshot.val();
-        //console.log(snapshot.val());
     });
+
     firebase.database().ref(userId+'/name').on('value', function(snapshot){
         $scope.name = snapshot.val();
-        //console.log(snapshot.val());
     });
 
     $scope.redirect = function(){
-        console.log("In redirect Method");
-        //window.location = "#/list.htm";
         $location.path('#/list.htm');
-
     }
+
     $scope.redirect2 = function(){
-        console.log("In redirect2 Method");
-        //window.location = "#/list.htm";
         $location.path('#/editSkills.html');
-
     }
 
-    firebase.database().ref.orderByChild("skills").equalTo("Java").on("child_added", function(snapshot) {
-        console.log(snapshot.key());
-    });
+    $scope.signOut = function () {
+        Auth.$signOut();
 
+    }
 
 }]);
 
+TeamApp.controller('listController' ,['$scope', '$route', '$routeParams', '$location' , function($scope , $route, $routeParams, $location) {
 
-TeamApp.controller('listController' ,['$scope', '$http','Service','$route', '$routeParams' ,
-    function($scope , $http, Service, $route, $routeParams ) {
+    $scope.param = $routeParams.id;
 
-        console.log("I am list");
-        $scope.param = $routeParams.id;
-        console.log($scope.param);
+    var allnames;
+    var allemails;
+    var allphones;
+
+
+    $scope.searchForSkills = function(value2){
+
+        var names = [];
+        var emails = [];
+        var phones = [];
+
+        value2 = value2.toLowerCase();
+        value2 = value2.trim();
+
+        firebase.database().ref().on('value', function(snapshot){
+
+            snapshot.forEach(function (childSnap) {
+
+                for(var i=0;i<childSnap.val().skills.length;i++){
+
+                    if(childSnap.val().skills[i]==value2) {
+
+                        names.push(childSnap.val().name);
+                        emails.push(childSnap.val().email);
+                        phones.push(childSnap.val().phone);
+
+                        console.log(childSnap.val().name);
+                        break;
+                        /*
+                        console.log(childSnap.val().email);
+                        console.log(childSnap.val().phone);
+                        */
+                    }
+
+                }
+
+            });
+            console.log(names);
+        });
+
+        $scope.allnames = names;
+        $scope.allemails = emails;
+        $scope.allphones = phones;
+
+    }
+
+
 
     }]);
 
 
-TeamApp.controller('editController' ,['$scope', '$http','Service','$route', '$routeParams' ,
-    function($scope , $http, Service, $route, $routeParams ) {
+TeamApp.controller('editController' ,['$scope', '$route', '$routeParams', '$location' , function($scope , $route, $routeParams, $location) {
 
-        var userId = firebase.auth().currentUser.uid;
-        var skills;
+    var userId = firebase.auth().currentUser.uid;
+    var skills;
+    $scope.param = $routeParams.id;
 
-        firebase.database().ref(userId+'/skills').on('value', function(snapshot){
+    firebase.database().ref(userId+'/skills').on('value', function(snapshot){
             $scope.skills = snapshot.val();
-            //console.log(snapshot.val());
+    });
+
+    var phone,name,email;
+
+    firebase.database().ref(userId+'/email').on('value', function(snapshot){
+        $scope.email = snapshot.val();
+    });
+
+    firebase.database().ref(userId+'/phone').on('value', function(snapshot){
+        $scope.phone = snapshot.val();
+    });
+
+    firebase.database().ref(userId+'/name').on('value', function(snapshot){
+        $scope.name = snapshot.val();
+    });
+
+
+    $scope.addThisSkill = function(value3){
+
+        value3 = value3.toLowerCase();
+        value3 = value3.trim();
+
+        $scope.skills.push(value3);
+
+        firebase.database().ref(userId).set({
+            email: $scope.email ,
+            name : $scope.name ,
+            phone : $scope.phone ,
+            skills : $scope.skills
         });
 
-        console.log("I am edit");
-        $scope.param = $routeParams.id;
-        console.log($scope.param);
+    }
+
+
+    $scope.removeThisSkill = function(value4){
+
+        value4 = value4.toLowerCase();
+        value4 = value4.trim();
+
+        var newSkills = [];
+
+        for(var i=0;i<$scope.skills.length;i++){
+            var item = $scope.skills[i];
+            if(item!=value4){
+                newSkills.push(item);
+            }
+        }
+
+        $scope.skills=newSkills;
+
+        firebase.database().ref(userId).set({
+            email: $scope.email ,
+            name : $scope.name ,
+            phone : $scope.phone ,
+            skills : $scope.skills
+        });
+
+    }
 
     }]);
